90c74542e92bbe61f3ef4225abfebcd8a2c4abce
Xuanhao Zhao <83867398+XuanhaoZhao@users.noreply.github.com>
2021-08-10 10:43:17 +0800

    完整项目
    外星人入侵在书中的完整项目，可以正常运行，可以加以改动，成为升级版

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,8 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -20,14 +22,20 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
+
         #创建一个用于存储游戏统计信息的实例
+        # 并创建记分牌
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        #创建play按钮
+        self.play_button = Button(self, "Play")
 
         #设置背景颜色
         self.bg_color = (230, 230, 230)
@@ -55,7 +63,32 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    def _check_play_button(self, mouse_pos):
+        """在玩家单击Play按钮时开始游戏"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            #重置游戏设置
+            self.settings.initialize_dynamic_settings()
+            #重置游戏统计信息
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
+            #清空余下的外星人和子弹
+            self.aliens.empty()
+            self.bullets.empty()
+
+            #创建一群新的外星人并让飞船居中
+            self._create_fleet()
+            self.ship.center_ship()
+
+            #隐藏鼠标光标
+            pygame.mouse.set_visible(False)
     def _check_keydown_events(self, event):
         """响应按键"""
         if event.key == pygame.K_RIGHT:
@@ -91,7 +124,12 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
 
+        #显示得分
+        self.sb.show_score()
 
+        #如果游戏处于非活动状态， 就绘制play按钮
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         #让最近绘制的屏幕可见
         pygame.display.flip()
     def _update_bullets(self):
@@ -114,10 +152,21 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
         if not self.aliens:
             #删除现有的子弹并创建一群外星人
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+
+            #提高等级
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """创建外星人群"""
@@ -179,8 +228,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """响应飞船被外星人撞到"""
         if self.stats.ships_left > 0:
-            #将ship_left减1
+            #将ship_left减1并更新记分牌
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             #清空剩下的外星人和子弹
             self.aliens.empty()
@@ -194,6 +244,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
     def _check_aliens_bottom(self):
         """检查是否有外星人到达屏幕底端"""
         screen_rect = self.screen.get_rect()
--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -8,6 +8,12 @@ class GameStats:
 		#让游戏刚启动时处于非活动状态
 		self.game_active = False
 
+		#任何情况下都不应重置最高得分
+		self.high_score = 0
+
+
 	def reset_stats(self):
 		"""初始化在游戏"""
 		self.ships_left = self.settings.ship_limit
+		self.score = 0
+		self.level = 1
